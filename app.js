ca97cfad62b199cff1bcc035fca1f79b7b09ff16
minzarddillah <minzard.dillah@gmail.com>
2018-09-09 21:28:34 +0700

    deploy server

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const session = require('express-session')
 const routes = require('./routes/')
 const app = express()
 const db = mongoose.connection
+const port = process.env.PORT || 3000
 
 mongoose.connect('mongodb://minzard:minzard1@ds249992.mlab.com:49992/to-do-fancy')
 db.on('error', console.error.bind(console, 'connection error'))
@@ -16,17 +16,11 @@ db.once('open', function(){
 app.use(cors())
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
-app.use(session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-}))
+
 
 app.use('/',routes)
 
 
-app.listen(3000, ()=>{
-    console.log(`http://localhost:3000/`)
+app.listen(port, ()=>{
+    console.log(`running at port: ${port}`)
 })
-
-// mlab.com
